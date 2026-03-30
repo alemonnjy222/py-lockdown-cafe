48edce274b00d04189eda5799e4d6e9795003023
alemonnjy222 <71006485+alemonnjy222@users.noreply.github.com>
2026-03-30 08:48:02 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ from app.errors import (
     NotWearingMaskError,
 )
 
-def go_to_cafe(friends: list, cafe: str):
+def go_to_cafe(friends: list, cafe: Cafe) -> str:
     masks_to_buy = 0
 
     for friend in friends:
